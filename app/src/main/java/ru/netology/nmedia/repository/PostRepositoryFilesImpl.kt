092e8bb301409e55de15f06b1ad182988f8cd6b7
Venom <test@neto.com>
2025-04-22 01:20:01 +0300

    Intents_Error correction

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFilesImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFilesImpl.kt
@@ -21,29 +21,6 @@ class PostRepositoryFilesImpl(private val context: Context) : PostRepository {
             field = value
             sync()
         }
-//        Post(
-//        id = 1,
-//        author = "Нетология. Университет интернет-профессий будущего",
-//        video = null,
-//        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-//        published = "21 мая в 18:36",
-//        likedByMe = false,
-//        toShare = false,
-//        numberLikes = 1099,
-//        shared = 1099,
-//        numberViews = 1100
-//    ),
-//        Post(id = 2,
-//        author = "Нетология. Университет интернет-профессий будущего",
-//        video = "https://rutube.ru/video/8b07ab2281235edaec284f914a355817/",
-//        content = null,
-//        published = "21 мая в 18:36",
-//        likedByMe = false,
-//        toShare = false,
-//        numberLikes = 1099,
-//        shared = 1099,
-//        numberViews = 1100
-//    )
 
     private val data = MutableLiveData(posts)
 
